7d4d6e96989c149160633667eb17e9d26c49b187
H.T. Schellingerhout <henri@savvii.com>
2018-04-17 13:46:42 +0200

    Bump version

--- warpdrive.php
+++ warpdrive.php
@@ -3,7 +3,7 @@
  * Plugin name: Warpdrive
  * Plugin URI: https://github.com/Savvii/warpdrive
  * Description: Hosting plugin for Savvii
- * Version: 2.9.0
+ * Version: 2.9.1
  * Author: Savvii <support@savvii.com>
  * Author URI: https://www.savvii.com
  * License: GPL-3.0-only
