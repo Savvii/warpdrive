446589fb4107bfc467d9ce59d3b3fc2fc9076699
Matthias Calis <matthiascalis@gmail.com>
2019-07-03 23:35:30 +0200

    % Fixed missing argument in prepare statement.

--- src/class-database-size-plugin.php
+++ src/class-database-size-plugin.php
@@ -52,8 +52,8 @@ class DatabaseSizePlugin {
      		table_name AS `Table`, 
      		round(((data_length + index_length) / 1024 / 1024), 2) `Size in MB` 
 			FROM information_schema.TABLES
-			WHERE table_schema = {$systemname}
-			ORDER BY (data_length + index_length) DESC;" ));
+			WHERE table_schema = %s
+			ORDER BY (data_length + index_length) DESC;", $systemname ));
 		?>
 		<h2>View database table sizes</h2>
 		<?php 
