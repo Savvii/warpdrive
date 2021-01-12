9025df84e4f7e1b03902bcc472d2d8accfb69884
Joost Vermeulen <joost@jover.net>
2021-01-12 10:45:47 +0100

    SA-7317: Flush OpCache   - added logic to flush the php OpCache, when you flush the caches   - wrapper around getenv() in Options class, so we can check $_ENV and $_SERVER (used if you inject env vars with dotenv)

--- src/class-cache-flusher.php
+++ src/class-cache-flusher.php
@@ -28,6 +28,9 @@ class CacheFlusher {
      * @return bool True on success
      */
     function flush() {
+        // Flush the OpCache
+        $this->flush_opcache();
+
         // Flush the cache
         $result = $this->api->cache_flush();
 
@@ -40,6 +43,9 @@ class CacheFlusher {
      * @return bool True on success
      */
     function flush_domain() {
+        // Flush the OpCache
+        $this->flush_opcache();
+
         // Get the current domain
         $domain = wp_parse_url( get_site_url() );
         $host = isset( $domain['host'] ) ? $domain['host'] : '';
@@ -49,5 +55,17 @@ class CacheFlusher {
 
         // Call API and check response code
         return $result->success();
+    }
+
+    /**
+     * Flush the OpCache if enabled
+     */
+    private function flush_opcache()
+    {
+        $opcache_status = opcache_get_status();
+
+        if ($opcache_status && is_array($opcache_status) && $opcache_status['opcache_enabled']) {
+            opcache_reset();
+        }
     }
 }
