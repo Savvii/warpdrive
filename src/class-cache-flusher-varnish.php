78b7e69276d85c8243c602724b13e00e33daec75
Joost Vermeulen <joost@jover.net>
2021-01-15 11:18:22 +0100

    Create a class per cache, CacheFlusher now calls the flusher class for each class

---
+++ src/class-cache-flusher-varnish.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Savvii;
+
+/**
+ * Class SavviiCacheFlusher
+ * Sends cache flush request to the API
+ */
+class CacheFlusherVarnish implements CacheFlusherInterface {
+
+    /**
+     * Api object instance
+     * @private Api
+     */
+    private $api;
+
+    /**
+     * Constructor
+     * @param array $args Options
+     */
+    public function __construct() {
+        // Create SavviiApi instance for API communication
+        $this->api = new Api();
+    }
+
+    /**
+     * Flush cache
+     * @return bool True on success
+     */
+    public function flush() {
+        // Flush the cache
+        $result = $this->api->cache_flush();
+
+        // Call API and check response code
+        return $result->success();
+    }
+
+    /**
+     * Flush cache for a specific domain
+     * @param null $domain
+     * @return bool True on success
+     */
+    public function flush_domain($domain = null) {
+
+        // Flush the domain cache
+        $result = $this->api->cache_flush( $domain );
+
+        // Call API and check response code
+        return $result->success();
+    }
+
+}
